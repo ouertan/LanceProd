cb5131e85a992f5d6628db085ca84d72ccd572a5
heeba O <161805168+ouertan@users.noreply.github.com>
2026-07-07 20:17:27 +0100

    Add colors constant

---
+++ NOVAGESTION-COMPLETE/src/constants/colors.js
@@ -0,0 +1,12 @@
+export const Colors = {
+  navy: "#0B2545",
+  blueDeep: "#13315C",
+  blueMid: "#2C5F9E",
+  sky: "#3E92CC",
+  ice: "#EAF2FB",
+  white: "#FFFFFF",
+  amber: "#E8A33D",
+  green: "#2E9E6B",
+  red: "#C0455B",
+  ink: "#5B7089",
+};
